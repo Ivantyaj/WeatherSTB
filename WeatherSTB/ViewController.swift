16221e2d3131ec3db3c099771b1fc9a465186166
Ivantyaj <39460276+Ivantyaj@users.noreply.github.com>
2020-04-18 15:30:30 +0300

    Get json

--- WeatherSTB/ViewController.swift
+++ WeatherSTB/ViewController.swift
@@ -27,8 +27,25 @@ extension ViewController: UISearchBarDelegate{
         let urlString = "http://api.weatherstack.com/current?access_key=06c166e90818aa7aa076253281bc1e5f&query=\(searchBar.text!)"
         
         let url = URL(string: urlString)
+        
+        var locationName: String?
+        var currentTemperature: Double?
+        
         let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
-            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: AnyObject]
+                
+                if let location = json["location"] {
+                    locationName = location["name"] as? String
+                }
+                
+                if let current = json["current"] {
+                    currentTemperature = current["temperature"] as? Double
+                }
+                
+            } catch let jsonError {
+                print(jsonError)
+            }
         }
         
         task.resume()
